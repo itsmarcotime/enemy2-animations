06d75f7ea149b3f548b3e16e38b1685c5b36884f
Marco <patrickstar1281@yahoo.com>
2023-02-11 04:19:53 -0600

    enemy2 movement done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -21,11 +21,15 @@ class Enemy {
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
 
     update() {
         this.x -= this.speed;
-        // this.y += Math.random() * 5 - 2.5;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
         if (this.x + this.width < 0) {
             this.x = canvas.width;
         }
